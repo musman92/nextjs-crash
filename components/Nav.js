aa373335d870f203dd03c0e87c665ec5ceb873cb
ali <ali@alis-MacBook-Pro.local>
2021-05-02 21:47:24 +0500

    routing, layout, api calls

---
+++ components/Nav.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import styles from '../styles/Nav.module.css'
+
+const Nav = ({children}) => {
+  return (
+    <nav className={styles.nav}>
+        <ul>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/about">About</Link>
+            </li>
+        </ul>
+
+    </nav>
+  )
+}
+
+export default Nav
